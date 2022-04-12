3c3e87b4c5c45470112f2ddb36d0e8df8822a94e
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-04-12 18:58:20 +0200

    Added navbar by react bootstrap and responsive

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -3,16 +3,16 @@ import React from "react";
 const Expert = ({ expert }) => {
   const { name, img } = expert;
   return (
-    <div class=" g-5 col-sm-12 col-md-6 col-lg-4">
-      <div class="card" style={{ width: "18rem" }}>
-        <img src={img} class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">{name}</h5>
-          <p class="card-text">
+    <div className=" g-5 col-sm-12 col-md-6 col-lg-4">
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer.
           </p>
-          <a href="#" class="btn btn-primary">
+          <a href="#" className="btn btn-primary">
             Go somewhere
           </a>
         </div>
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <div>
-      <footer>
-        <p>
-          <small>copyright @ /dynamic year/</small>
-        </p>
-      </footer>
-    </div>
+    <footer className="text-center mt-5">
+      <p>
+        <small className="fw-bold">copyright @ {year}</small>
+      </p>
+    </footer>
   );
 };
 
