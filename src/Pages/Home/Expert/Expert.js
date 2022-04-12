ae6a557fef75ed7d2b6bb0df12d8bd2268cd649d
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-04-12 18:12:17 +0200

    responsive with bootstrapand added carousel by react bootstrap

---
+++ src/Pages/Home/Expert/Expert.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Expert = ({ expert }) => {
+  const { name, img } = expert;
+  return (
+    <div class=" g-5 col-sm-12 col-md-6 col-lg-4">
+      <div class="card" style={{ width: "18rem" }}>
+        <img src={img} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{name}</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Expert;
